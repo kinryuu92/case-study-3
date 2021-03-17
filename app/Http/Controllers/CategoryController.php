fe7242984580af36234552da03307df918a497da
Phi Minh Tu <phiminhtu287@gmail.com>
2021-03-17 23:13:20 +0700

    case-study-3

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Components\Recusive;
 
 class CategoryController extends Controller
 {
+    private $category;
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+
+
     public function create(){
-        dd('created');
-        return view('category.add');
+//        $data = $this->category->all();
+//        $recusive = new Recusive($data);
+//        $htmlOption = $recusive->categoryRecusive();
+        $htmlOption = $this->getCategory($parentId = '');
+        return view('category.add', compact('htmlOption'));
+    }
+
+    public function index(){
+        $categories = $this->category->latest()->paginate(5);
+
+        return view('category.index', compact('categories'));
+    }
+
+    public function store(Request $request){
+        $this->category->create([
+            'name' => $request->name,
+            'parent_id'=> $request->parent_id,
+            'slug' => $request->name
+        ]);
+        return redirect()->route('categories.index');
+    }
+
+    public function getCategory($parentId){
+        $data = $this->category->all();
+        $recusive = new Recusive($data);
+        $htmlOption = $recusive->categoryRecusive($parentId);
+        return $htmlOption;
+    }
+
+    public function edit($id){
+        $category = $this->category->find($id);
+        $htmlOption = $this->getCategory($category->parent_id);
+        return view('category.edit', compact('category', 'htmlOption'));
+
+    }
+
+    public function update($id, Request $request ){
+        $this->category->find($id)->update([
+            'name' => $request->name,
+            'parent_id'=> $request->parent_id,
+            'slug' => $request->name
+        ]);
+        return redirect()->route('categories.index');
+
+    }
+
+    public function delete($id){
+
     }
 
 }
--- resources/views/partials/siderbar.blade.php
+++ resources/views/partials/siderbar.blade.php
@@ -1,24 +1,19 @@
-<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
             </div>
         </div>
 
-        <!-- SidebarSearch Form -->
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
                 <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
@@ -30,14 +25,10 @@
             </div>
         </div>
 
-        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                     with font-awesome or any other icon font library -->
-
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="{{ route('categories.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Danh muc san pham
@@ -47,7 +38,5 @@
                 </li>
             </ul>
         </nav>
-        <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
 </aside>
