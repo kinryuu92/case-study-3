d6212db67f915341bc87214062b258ebaa1140cf
Phi Minh Tu <phiminhtu287@gmail.com>
2021-03-24 15:20:37 +0700

    case-study-3

---
+++ resources/views/admin/menus/add.blade.php
@@ -0,0 +1,39 @@
+
+@extends('layouts.admin')
+@section('title')
+    <title>Trang chu</title>
+@endsection
+@section('content')
+    <div class="content-wrapper">
+        @include('partials.content-header', ['name'=>'menus', 'key'=>'Add'])
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form action="{{ route('menus.store') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label>Tên menus</label>
+                                <input type="text" class="form-control"
+                                       placeholder="Nhập tên menu " name="name">
+                            </div>
+                            <div class="form-group">
+                                <label>Chọn menus cha</label>
+                                <select class="form-control" name="parent_id">
+                                    <option value="0">Chọn menu cha</option>
+                                    {!! $optionSelect !!}
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
+
